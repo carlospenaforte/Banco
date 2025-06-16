b802147fb34901cd7d2c80a8e650d7d48e33b905
carlospenaforte <carlosedu_1132@hotmail.com>
2025-06-15 21:49:38 -0300

    Set datacontext

---
+++ Banco/Context/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Banco.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banco.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.AccountOrigin)
+                .WithMany(a => a.TransactionOrigin)
+                .HasForeignKey(t => t.AccountOriginId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Account>()
+                .HasMany(t => t.TransactionDestination)
+                .WithOne(a => a.AccountDestination)
+                .HasForeignKey(t => t.AccountDestinationId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
