4ef46a64b563fa67433285b72ce9e4ed23c2eadf
carlospenaforte <carlosedu_1132@hotmail.com>
2025-06-15 21:52:17 -0300

    Builder DataContext in Program.cs and json connection to the database bancoconnection

--- Banco/Program.cs
+++ Banco/Program.cs
@@ -1,3 +1,6 @@
+using Banco.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BancoConnection")));
 
 var app = builder.Build();
 
